3d0352c6b25a520612ea0ca9477ca393137bb0bc
Madhura M <madhura.26m@gmail.com>
2021-04-09 00:49:53 +0530

    first commit

---
+++ test.c
@@ -0,0 +1,17 @@
+/**
+ * @file test.c
+ * @author madhura
+ * @brief 
+ * @version 0.1
+ * @date 2021-04-09
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+
+#include<stdio.h>
+int main(){
+    printf("Hello world");
+    return 0;
+}
